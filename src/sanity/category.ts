e32ebf2e79a3e331f7da1e8476f5730f5537e54c
MAC <mac@MACs-MacBook-Pro.local>
2025-04-20 02:57:34 +0500

    Apr 19

---
+++ src/sanity/category.ts
@@ -0,0 +1,14 @@
+import { title } from "process";
+
+export const category ={
+    name: "category",
+    title: "Category",
+    type : "document",
+    fields : [
+        {
+        name : "name",
+        title : "category name",
+        type : "string"
+    }
+    ]
+}
