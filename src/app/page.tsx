96af8a73c12f21deeb562207518641dba02bc2ab
MAC <mac@MACs-MacBook-Pro.local>
2025-04-20 01:18:48 +0500

    Apr 19

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
 import Image from "next/image";
 import {client} from "@/sanity/lib/client"
+import { Interface } from "readline";
 
 export const getProductData = async() =>{
-  const result = await client.fetch(`*[_type=='product']`);
+  const result = await client.fetch(`*[_type=='product']{
+    name,Description}`);
   return result;
+};
+
+
+
+interface Iproduct {
+  name: string;
+  description: string;
 }
 
 export default async function Home() {
-  const data = await getProductData()
+  const data:Iproduct[] = await getProductData()
   console.log(data);
 
   return (
     <div >
-    
+    {data.map((item)=>(
+      <h1> {item.name} </h1>
+    ))}
 
     </div>
   );
